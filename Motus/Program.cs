f1f4013e936a060511661a2b82372158fb4a18dd
Mika Dal Cero <dalcero.mika.1206@outlook.be>
2024-09-24 15:59:31 +0200

    Update Vue

--- Motus/Program.cs
+++ Motus/Program.cs
@@ -1,7 +1,61 @@
-﻿namespace Motus {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Metrics;
+
+namespace Motus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Essai;
+            bool fin = false;
+
+            Letter firstLetter = start();
+            Console.WriteLine($"{firstLetter.letter} _ _ _ _ _ _ _\n");
+
+            while (!fin)
+            {
+
+                Console.WriteLine("Entrez un mot de 8 lettres : ");
+                Essai = Console.ReadLine();
+
+                // appelle fct pour comparer les lettres
+                Letter[] mots = Compare(Essai);
+
+                for (int i = 0; i < 8; i++)
+                {
+                    if (mots[i].isWellPlaced)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.Write(mots[i].letter);
+                    }
+                    else if (mots[i].isWrongPlaced)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.Write(mots[i].letter);
+                    }
+                    else if (mots[i].isNotPresent)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Write("_");
+                    }
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\n\n");
+
+                int cpt = 0;
+                foreach (Letter l in mots)
+                {
+                    if (!l.isWellPlaced)
+                    {
+                        cpt++;
+                    }
+                }
+
+                if (cpt == 8)
+                    fin = true;
+            }
         }
     }
 }
+
