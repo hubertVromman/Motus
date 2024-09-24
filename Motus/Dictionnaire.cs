1f20d6ff99d15684f8aecf224f92b5fdb82a9f3e
Louis Blancke <louis.blancke1711@gmail.com>
2024-09-24 16:32:33 +0200

    Recherche mot + dico

---
+++ Motus/Dictionnaire.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motus
+{
+    public enum Dictionnaire
+    {
+        agalaxie,
+        bananier,
+        sertirai,
+        garderie,
+        gazoline,
+        faillite,
+        forreuse,
+        fibromes,
+        tangible,
+        tapisser,
+        tatonner,
+        terminal,
+        thermale,
+        thriller,
+        saboteur,
+        saccager,
+        saliviez,
+        sapristi,
+        semoules,
+        serpents
+    }
+}
